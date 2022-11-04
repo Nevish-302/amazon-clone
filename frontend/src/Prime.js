d86eba18dabe3dcc3789959df7851f324399f374
Vaani <vaanipathariya@gmail.com>
2022-11-04 22:22:26 +0530

    routes for account pages

---
+++ frontend/src/Prime.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Prime=()=>{
+return(
+    <div>hello i am prime</div>
+)
+}
+export default Prime;
---
+++ frontend/src/main_components/Address.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Address=()=>{
+return(
+    <div>hello i am Address</div>
+)
+}
+export default Address;
