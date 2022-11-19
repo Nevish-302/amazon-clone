9908472bd67b4e0d40541ec8fdf4d45a9f3697a5
Vaani <vaanipathariya@gmail.com>
2022-11-19 14:33:19 +0530

    sending data

--- frontend/src/Login2.js
+++ frontend/src/Login2.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import white from "./images/white_logo.jpg"
 import "./main_components/login.css";
 import {Link} from "react-router-dom";
 const Login2=()=>{
+    const segment=window.location.href.split("=").pop();
+    const [password,setPassword]=useState('');
+    const handleChange_pass=event=>{
+        setPassword(event.target.value)
+    }
+    const handleClick = event => {
+        event.preventDefault();
+        //password
+      };
     return(
         <div className="whole">
         <Link to="/"><img src={white}></img></Link>
         <div className="inputs">
             <h1>Sign in</h1>
-            <p>Here the given email id will be displayed</p>
+            <p>{segment} .Change</p>
             <label>Password</label><br></br>
-            <input type="password" className="password"></input><br></br>
+            <input type="password" className="password" onChange={handleChange_pass}></input><br></br>
             <Link to="/"><button className="continue">Sign in</button></Link>
             <input type="checkbox"></input>Keep me signed in. Details
         </div>
